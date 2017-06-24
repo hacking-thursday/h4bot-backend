2d6fd1f9176e98ac831f20bcb5575c64123e8d18
yan <a0726h77@gmail.com>
2017-06-24 14:30:16 +0800

    指定搜尋日期

--- h4bot.py
+++ h4bot.py
@@ -54,10 +54,14 @@ def checkin():
 def attendee():
     attendee = []
 
-    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    if request.args.get('date'):
+        d = request.args.get('date')
+        print 111
+    else:
+        d = datetime.datetime.now().strftime("%Y-%m-%d")
 
     cur = g.db.cursor()
-    cur.execute("SELECT DISTINCT checkin.user_id, users.*, n.* FROM checkin LEFT OUTER JOIN users ON users.user_id=checkin.user_id LEFT OUTER JOIN (SELECT MAX(nick.id), nick.* FROM nick GROUP BY nick.user_id) n ON checkin.user_id=n.user_id WHERE checkin.datetime LIKE '%s%%'" % today)
+    cur.execute("SELECT DISTINCT checkin.user_id, users.*, n.* FROM checkin LEFT OUTER JOIN users ON users.user_id=checkin.user_id LEFT OUTER JOIN (SELECT MAX(nick.id), nick.* FROM nick GROUP BY nick.user_id) n ON checkin.user_id=n.user_id WHERE checkin.datetime LIKE '%s%%'" % d)
     #cur.execute("SELECT DISTINCT checkin.user_id, users.*, n.* FROM checkin LEFT OUTER JOIN users ON users.user_id=checkin.user_id LEFT OUTER JOIN (SELECT MAX(nick.id), nick.* FROM nick GROUP BY nick.user_id) n ON checkin.user_id=n.user_id WHERE checkin.datetime LIKE '2017-06-01%%'")
     _ = cur.fetchall()
 
